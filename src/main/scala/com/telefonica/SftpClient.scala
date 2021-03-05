92c7c0677c423394f6928c694c44b6914a6035cf
hdp <gabriel.alaniz89@gmail.com>
2021-03-05 11:29:32 -0300

    fixes

--- src/main/scala/com/telefonica/SftpClient.scala
+++ src/main/scala/com/telefonica/SftpClient.scala
@@ -1,118 +1,126 @@
 package com.telefonica
 
-import com.jcraft.jsch.SftpATTRS
 import com.jcraft.jsch.JSch
 import com.jcraft.jsch.ChannelSftp
+import java.util.ArrayList
 import java.io.File
-import scala.collection.mutable.ListBuffer
+import org.apache.log4j.Logger
+import java.io.FileInputStream
 
 class SftpClient {
-
+  @transient
+  lazy val log: Logger = Logger.getLogger(getClass.getName)
   
-  def listFiles(username: String, password: String, host: String, port: Int, path: String){
+  def setupConnection(username: String, host: String, port: String): ChannelSftp = {
     val jsch = new JSch()
+    var sftp = new ChannelSftp()
+    val jschSession = jsch.getSession(username,host, port.asInstanceOf[Int])
+    val config = new java.util.Properties()
+    config.put("StrictHostKeyChecking", "no")
+    jschSession.setConfig(config)
+    jschSession.connect()
+    if(jschSession.isConnected()){
+      sftp = (jschSession.openChannel("sftp")).asInstanceOf[ChannelSftp]
+      sftp.connect()
+      log.info("Conexão exitosa")
+    } else {
+     throw new Exception(s"Erro ao tentar conectar no SFTP") 
+    }
+    return sftp
+  }
+  
+  def listarArquivos(sftp:ChannelSftp, path:String){
     try{
-      val jschSession = jsch.getSession(username, host, port)
-      val config = new java.util.Properties()
-      config.put("StrictHostKeyChecking", "no")
-      jschSession.setConfig(config)
-      jschSession.setPassword(password)
-      jschSession.connect()
-      if(jschSession.isConnected()){
-        val sftp = (jschSession.openChannel("sftp")).asInstanceOf[ChannelSftp]
-        sftp.connect()
-        if(sftp.isConnected()){
-          println("Arquivos do Diretòrio:")
-          val lista = null
-          listDirectory(sftp, path, lista)
-        	sftp.disconnect()
-        }
-        jschSession.disconnect()
+      if(sftp.isConnected()){
+    	  val files:Vector[ChannelSftp#LsEntry] = sftp.ls(path).asInstanceOf
+			  var dir = new ArrayList[String]
+			  for(f <- files){
+				  if(!f.getAttrs.isDir()){
+					  dir.add(path+"/"+f.getFilename) // guardo los archivos finales
+				  } else if(f.getAttrs.isDir()){
+					  listarArquivos(sftp, path+"/"+f.getFilename)
+			    }
+		    }  
       } else {
-       throw new Exception(s"Erro ao tentar conectar no SFTP: $username@$host") 
+        throw new Exception(s"Erro ao tentar conectar no SFTP")
       }
     } catch {
-      case e: Throwable =>
-      e.printStackTrace()
+      case e: Throwable => e.printStackTrace()
+      log.error("Erro ao tentar conectar no SFTP: " + e.getMessage)
     }
   }
   
-  def listDirectory(sftp:ChannelSftp, path:String, lista:List[File]){
-    val lista:List[File] = (sftp.ls(path))asInstanceOf; // pasa directorio, guardo su contenido
-    var listBuf = new ListBuffer[File]()
-      for(l <- lista){
-        if (!l.isDirectory()){
-          val file = new File(path + "/" + l.getName)
-          listBuf += file
-        } else if(!l.getName.equals(".") && !l.getName.equals("..")){
-          listDirectory(sftp, path+"/"+l.getName, listBuf.toList) 
-        }
-      }
-  }
-  
-  def downloadFromServer(username: String,
-      password: String, host: String, port: Int, path:String, filename:String, diretorio:String){
-    val jsch = new JSch()
+  def downloadDesdeSftp(sftp: ChannelSftp, localPath: String, remotePath: String){
     try{
-      val jschSession = jsch.getSession(username, host, port)
-      val config = new java.util.Properties()
-      config.put("StrictHostKeyChecking", "no")
-      jschSession.setConfig(config)
-      jschSession.setPassword(password)
-      jschSession.connect()
-      if(jschSession.isConnected()){
-        val sftp = (jschSession.openChannel("sftp")).asInstanceOf[ChannelSftp]
-        sftp.connect()
-        if(sftp.isConnected()){
-          
-//          So um arquivo
-//          sftp.get(filename, diretorio)
-          
-//          Todos os arquivos
-//          sftp.get("*", diretorio)
-          println("\nTransferência concluída")
-          
-        	sftp.disconnect()
+    	if(sftp.isConnected()){
+      	var localDir:File = localPath.asInstanceOf[File]
+        if(!localPath.asInstanceOf[File].isDirectory()){
+          localDir.mkdir()
         }
-        jschSession.disconnect()
+        localDir = sftp.lcd(localPath).asInstanceOf[File]
+        getRemoteFile(sftp, remotePath, localDir)
+//        getRemoteFile(sftp, remotePath, localDir) // localDir: String
+        
+        disconnect(sftp)
       } else {
-       throw new Exception(s"Erro ao tentar conectar no SFTP: $username@$host") 
+        throw new Exception(s"Erro ao tentar conectar no SFTP")
       }
     } catch {
-      case e: Throwable =>
-      e.printStackTrace()
+      case e: Throwable => e.printStackTrace()
     }
   }
   
-  def uploadLocalFile(username: String,
-      password: String, host: String, port: Int, filename:String){
-    val jsch = new JSch()
+  def uploadDesdeLocal(sftp: ChannelSftp, localFile: String, remotePath: String){
     try{
-      val jschSession = jsch.getSession(username, host, port)
-      val config = new java.util.Properties()
-      config.put("StrictHostKeyChecking", "no")
-      jschSession.setConfig(config)
-      jschSession.setPassword(password)
-      jschSession.connect()
-      if(jschSession.isConnected()){
-        val sftp = (jschSession.openChannel("sftp")).asInstanceOf[ChannelSftp]
-        sftp.connect()
-        if(sftp.isConnected()){
-//          so um arquivo
-          sftp.put(filename)
-          
-          println("\nTransferência concluída")
-          
-        	sftp.disconnect()
-        }
-        jschSession.disconnect()
+      if(sftp.isConnected()){
+        putLocalFile(sftp, localFile.asInstanceOf[File], remotePath)
+        
+        disconnect(sftp)
       } else {
-       throw new Exception(s"Erro ao tentar conectar no SFTP: $username@$host") 
+        throw new Exception(s"Erro ao tentar conectar no SFTP")
       }
     } catch {
-      case e: Throwable =>
-      e.printStackTrace()
+      case e: Throwable => e.printStackTrace()
     }
   }
+  
+  def getRemoteFile(sftp: ChannelSftp, remotePath: String, localDir: File){
+//  def getRemoteFile(sftp: ChannelSftp, remotePath: String, localDir: String){
+    val files:Vector[ChannelSftp#LsEntry] = sftp.ls(remotePath).asInstanceOf
+    for(f <- files){
+      if(!f.getAttrs.isDir() && !(new java.io.File(localDir+"/"+f.getFilename)).exists){
+        sftp.get(f.getFilename.asInstanceOf[String], localDir.asInstanceOf[String])
+      } else if(f.getAttrs.isDir()) {
+        new File(localDir+"/"+f.getFilename).mkdir()
+        log.info(s"Pasta: ${f.getFilename} criada em: ${localDir}")
+        getRemoteFile(sftp, remotePath+"/"+f.getFilename, (localDir+"/"+f.getFilename).asInstanceOf[File])
+//        getRemoteFile(sftp, remotePath+"/"+f.getFilename, localDir+"/"+f.getFilename)
+      }
+    }
+  }
+  
+  def putLocalFile(sftp: ChannelSftp, localFile: File, remotePath: String){
+    if(localFile.isDirectory()){
+      sftp.mkdir(localFile.getName)
+      log.info(s"Pasta: ${localFile.getName} criada em: ${remotePath}")
+      var newRemotePath = remotePath+"/"+localFile.getName 
+      sftp.cd(remotePath)
+      for(f <- localFile.listFiles()){
+        putLocalFile(sftp, f, newRemotePath)
+      }
+    } else {
+      sftp.put(localFile.asInstanceOf[String], localFile.getName)
+      log.info(s"Copiando arquivo: ${localFile.getName} para: ${remotePath}")
+    }
+  }
+  
+  def disconnect(sftp: ChannelSftp){
+    if(sftp.isConnected()) {
+      sftp.disconnect()
+      sftp.exit()
+      log.info(s"Desconexao do servidor SFTP com sucesso")
+    }
+  }
+  
   
 }
